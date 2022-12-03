94035899e42c6a188f72a041e228af636c8f8a08
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-12-03 19:45:36 +0100

    Setup Framework styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 
+import "./assets/styles/index.scss";
 import App from './App';
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
